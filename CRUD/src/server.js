af970fa99e9873e5b6bbc1ed82b5258eedfdb74d
samikshk10 <samik.shakya8848@gmail.com>
2023-09-06 15:29:39 +0545

    crud operation on graphql

---
+++ CRUD/src/server.js
@@ -0,0 +1,167 @@
+const express = require("express");
+
+let userList = require("./mock-data.json");
+
+
+
+const {
+    graphqlHTTP
+} = require("express-graphql");
+
+const {
+    GraphQLID,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLSchema,
+    GraphQLNonNull
+} = require("graphql");
+
+const app = express();
+
+// user schema creation
+
+const userType = new GraphQLObjectType({
+    name: "UserType",
+    fields: () => ({
+        id: {
+            type: GraphQLID
+
+        },
+        name: {
+            type: GraphQLString
+        },
+        email: {
+            type: GraphQLString
+        },
+        message: {
+            type: GraphQLString
+        }
+
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQuery",
+    fields: {
+        users: {
+            type: new GraphQLList(userType),
+            resolve() {
+                return userList
+            }
+        },
+        user: {
+            type: userType,
+            args: {
+                id: {
+                    type: new GraphQLNonNull(GraphQLID),
+                },
+            },
+            resolve(parent, args) {
+                return userList.find((user) => user.id === parseInt(args.id))
+
+            }
+        }
+    }
+});
+
+
+
+//mutation
+
+const mutation = new GraphQLObjectType({
+    name: "mutations",
+    fields: {
+        //adding a user
+
+        addUser: {
+            type: userType,
+            args: {
+                name: {
+                    type: new GraphQLNonNull(GraphQLString)
+                },
+                email: {
+                    type: new GraphQLNonNull(GraphQLString)
+                }
+            },
+            resolve(parent, {
+                name,
+                email
+            }) {
+                const newUser = {
+                    name,
+                    email,
+                    id: Date.now()
+                }
+                userList.push(newUser)
+                return {
+                    ...newUser,
+                    message: "user added successully"
+                }
+            }
+        },
+
+        deleteUser: {
+            type: userType,
+            args: {
+                id: {
+                    type: GraphQLID
+                }
+            },
+
+            resolve(parent, {
+                id
+            }) {
+                const user = userList.find((u) => u.id === parseInt(id));
+                if (!user) throw new Error(`User with id: ${id} not found`)
+                userList = userList.filter(u => u.id !== parseInt(id))
+                return user
+            }
+        },
+        updateUser: {
+            type: userType,
+            args: {
+                id: {
+                    type: GraphQLID
+                },
+                name: {
+                    type: GraphQLString
+                },
+                email: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parelnt, {
+                id,
+                name,
+                email
+            }) {
+                const user = userList.find(u => u.id === parseInt(id));
+                if (!user) throw new Error(`User with id: ${id} not found`);
+                user.email = email;
+                user.name = name;
+                return user
+            }
+        }
+    }
+})
+
+
+const schema = new GraphQLSchema({
+    query: RootQuery,
+    mutation: mutation
+});
+
+// middleware
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+
+
+// server
+app.listen(8000, () => {
+    console.log('listening on server 8000');
+})
