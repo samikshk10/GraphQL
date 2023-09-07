1d758d78d421ae9ec155a717abe030fe2c955831
samikshk10 <samik.shakya8848@gmail.com>
2023-09-07 17:14:28 +0545

    graphql apollo sever

---
+++ Graphql-apollo/dist/server.js
@@ -0,0 +1,76 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import express from "express";
+import * as userList from "./mock-database/users.json" assert { type: "json" };
+const app = express();
+const typeDefs = `
+  type UserType {
+    id: ID
+    name: String
+    email: String
+    age: Int
+    message: String
+  }
+
+  type Query {
+    users: [UserType]
+    user(id: ID!): UserType
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!): UserType
+    deleteUser(id: ID!): UserType
+    updateUser(id: ID!, name: String, email: String): UserType
+  }
+`;
+// Define resolvers for Query and Mutation
+const resolvers = {
+    Query: {
+        users: () => userList,
+        user: (parent, args) => userList.find((user) => user.id === args.id),
+    },
+    Mutation: {
+        addUser: (parent, args) => {
+            const newUser = {
+                name: args.name,
+                email: args.email,
+                id: Date.now(),
+                age: args.age,
+            };
+            userList.push(newUser);
+            return {
+                ...newUser,
+                message: "user added successfully",
+            };
+        },
+        deleteUser: (parent, args) => {
+            const userIndex = userList.findIndex((u) => u.id === args.id);
+            if (userIndex === -1) {
+                throw new Error(`User with id: ${args.id} not found`);
+            }
+            const deletedUser = userList.splice(userIndex, 1)[0];
+            return deletedUser;
+        },
+        updateUser: (parent, args) => {
+            const user = userList.find((u) => u.id === args.id);
+            if (!user) {
+                throw new Error(`User with id: ${args.id} not found`);
+            }
+            if (args.name) {
+                user.name = args.name;
+            }
+            if (args.email) {
+                user.email = args.email;
+            }
+            return user;
+        },
+    },
+};
+const startServer = async () => {
+    const server = new ApolloServer({ typeDefs, resolvers });
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+    });
+    console.log(`🚀 Server running at port ${url}`);
+};
+startServer();
---
+++ dist/src/index.js
@@ -0,0 +1,177 @@
+// import { ApolloServer } from '@apollo/server';
+// import { startStandaloneServer } from '@apollo/server/standalone';
+// // A schema is a collection of type definitions (hence "typeDefs")
+// // that together define the "shape" of queries that are executed against
+// // your data.
+// const typeDefs = `#graphql
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+//   # This "Book" type defines the queryable fields for every book in our data source.
+//   type Book {
+//     title: String
+//     author: String
+//   }
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//   }
+// `;
+// const books = [
+//   {
+//     title: "The Awakening",
+//     author: "Kate Chopin"
+//   },
+//   {
+//     title: "City of Glass",
+//     author: "Paul Auster"
+//   }
+// ]
+// //Resolvers define how to fetch the types defined in your schema.
+// //This resolver retrieves books from the "books" array above.
+// const resolvers = {
+//   Query: {
+//     books: ()=> books,
+//   }
+// }
+// //The ApolloServer constructor requires two parameters: your schema
+// //definition and your set of resolvers.
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers
+// })
+// //Passing an ApolloServer instance to the `startStandAloneServer` function:
+// //1. creates an Express app
+// //2. installs your ApolloServer instance as middleware
+// //3. prepares your app to handle following requests
+// const {url} = await startStandaloneServer(server,{
+//   listen: { port: 4000}
+// })
+// console.log(`🚀 Server ready at: ${url}`)
+//#NOTE: revision
+// import { ApolloServer } from '@apollo/server'
+// import { startStandaloneServer } from '@apollo/server/standalone'
+// //hardcoded data
+// const books = [
+//   {
+//     title: "The Awakening",
+//     author: "Kate"
+//   }, {
+//     title: "City of Glass",
+//     author: "Paul"
+//   }
+// ]
+// //schema definitions
+// const typeDefs = `#graphql
+//   type Book{
+//     title: String
+//     author: String
+//   }
+//   type Query{
+//     books: [Book]
+//   }
+// `
+// //Resolver map
+// const resolvers = {
+//   Query: {
+//     books: () => books
+//   }
+// }
+// // pass schema and definition and resolvers to the
+// //Apolloserver constructor
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers
+// })
+// //launch the server
+// const {url} = await startStandaloneServer(server,{
+//   listen: {port: 4000}
+// })
+// console.log(`🚀 Server listening at port: ${url}`);
+//#NOTE: Resolver Chain
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+//hardcoded data
+const libraries = [
+    {
+        branch: "downtown",
+    },
+    {
+        branch: "riverside",
+    },
+];
+//the branch field of a book indicates which library has it in stock
+const books = [
+    {
+        title: "The Awakening",
+        author: "Kate Chopin",
+        branch: "riverside",
+    },
+    {
+        title: "City of Glass",
+        author: "Paul Auster",
+        branch: "downtown",
+    },
+];
+//Schema Definition
+const typeDefs = `#graphql
+
+  #A library has branch and books
+  type Library{
+    branch: String!
+    books: [Book!]
+  }
+
+  #A book has a title and author
+  type Book{
+    title: String
+    author: Author!
+  }
+
+  #An author has a name
+  type Author{
+    name: String!
+  }
+
+  #Queries can fetch a list of libraries
+  type Query {
+    libraries: [Library]
+  }
+
+`;
+//Resolver map
+const resolvers = {
+    Query: {
+        libraries() {
+            //return hardcoded array of libraries
+            return libraries;
+        },
+    },
+    Library: {
+        books(parent) {
+            //Filter the hardcoded of books to only include books that are located at the correct branch
+            return books.filter((book) => book.branch === parent.branch);
+        },
+    },
+    Book: {
+        //The parent resolver (Library.books) returns an object with the author's name in the "author" field.
+        //Return a JSON object containing the name, because this field expects an object.
+        author(parent) {
+            return {
+                name: parent.author,
+            };
+        },
+    },
+    //Because Book.author returns an object with a "name" field,
+    //Apollo server's default resolver for Author.name will work.
+    //We don't need to define them
+};
+//Pass schema definition and resolvers to the ApolloServer Constructor
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀 Server running at port ${url}`);
